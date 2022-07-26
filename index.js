2b53bb3fedfd5206e228fb824bef0b0f4bc725ac
Duy Pham <ericpham19@outlook.com>
2022-07-26 15:41:26 +1000

    Done lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+function changeText(){
+    const p = document.getElementById("text");
+    p.textContent = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    changeText();
+})
